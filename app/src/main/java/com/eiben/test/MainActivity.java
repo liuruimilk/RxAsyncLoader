f6058bb5a46051c734d41e8e8cc13d06ada502da
liumingrui <liumingrui@yongche.com>
2016-09-16 23:00:25 +0800

    update .gitignore

--- app/src/main/java/com/eiben/test/MainActivity.java
+++ app/src/main/java/com/eiben/test/MainActivity.java
@@ -1,10 +1,10 @@
 package com.eiben.test;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
