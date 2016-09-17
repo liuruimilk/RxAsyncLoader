4bb798e9f73a7bf4de35efc2ba24af832853ce82
liumingrui <liumingrui@yongche.com>
2016-09-17 18:12:09 +0800

    add time for log

--- app/src/main/java/com/eiben/test/MainActivity.java
+++ app/src/main/java/com/eiben/test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.eiben.test;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d("liumingrui","MainActivity oncreate");
+        Log.d("liumingrui","MainActivity oncreate" + SystemClock.currentThreadTimeMillis());
     }
 }
