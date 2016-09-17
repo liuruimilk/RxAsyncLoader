51668fbdc82fbe62da0b37e4aa9c0acd4e6641cb
liumingrui <liumingrui@yongche.com>
2016-09-17 21:46:23 +0800

    update

--- app/src/main/java/com/eiben/test/Person.java
+++ app/src/main/java/com/eiben/test/Person.java
@@ -1,18 +1,10 @@
 package com.eiben.test;
 
 /**
- * Created by liumingrui on 16/9/17.
+git  * Created by liumingrui on 16/9/17.
  */
 public class Person {
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String name;
 
 
 }
