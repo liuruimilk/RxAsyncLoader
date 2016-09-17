a7fb146dfc6eba9aa78320226514d029ef5296d6
liumingrui <liumingrui@yongche.com>
2016-09-17 17:59:24 +0800

    add log

--- app/src/main/java/com/eiben/test/MainActivity.java
+++ app/src/main/java/com/eiben/test/MainActivity.java
@@ -1,13 +1,15 @@
 package com.eiben.test;
 
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("liumingrui","MainActivity oncreate");
     }
 }
